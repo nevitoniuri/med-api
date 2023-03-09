d5f76b20cced7d8062397c503c8358ff42356819
Iuri Néviton <iurineviton@gmail.com>
2023-03-08 23:00:41 -0300

    readme

--- src/test/java/med/api/repository/MedicoRepositoryTest.java
+++ src/test/java/med/api/repository/MedicoRepositoryTest.java
@@ -3,9 +3,11 @@ package med.api.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class MedicoRepositoryTest {
 
     @Test
